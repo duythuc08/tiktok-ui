86cebdb0336aeacacf31bcfcf2b7a2213794b286
Nguyen Duy Thuc <duythuc08tt@gmail.com>
2025-08-14 09:45:38 +0700

    Dựng khung DefaultLayout dự án Tiktok Clone | Thực hành ReactJS tại F8

---
+++ src/components/Layout/index.js
@@ -0,0 +1,2 @@
+export {default as DefaultLayout} from './DefaultLayout' //export index.js của DefaultLayout dưới Layout
+export {default as HeaderOnly} from './HeaderOnly'
---
+++ src/pages/Sreach/index.js
@@ -0,0 +1,7 @@
+function Sreach() {
+    return (
+        <h2>Sreach Page</h2>
+    );
+}
+
+export default Sreach;
